372923f6a7f9c22a485cffa219499d55588be82d
akki <saikurisuto@gmail.com>
2021-03-26 12:19:32 +0900

    scratchレベルではできた

---
+++ make_job.R
@@ -0,0 +1,34 @@
+
+
+##############SCRATCH############################
+
+x_1 = c('t1.gjf,','o1.o')
+
+x_2 = c('t2.gjf,','o2.o')
+
+x_1_2 = rbind(x_1,x_2)
+
+x_1_2
+
+
+
+
+write.table(x_1_2,"C:/Users/simiz/Desktop/b_test/x_1_2.txt",col.names = F,row.names = F,quote = F)
+
+
+
+
+x = c("t1.gjf,","t2.gjf,","t3.gjf,")
+y = c("o1.o","o2.o","o3.o")
+
+xy = cbind(x,y)
+
+xy
+
+write.table(xy,"C:/Users/simiz/Desktop/b_test/test_3.bcf",col.names = F,row.names = F,quote = F)
+
+#########################################
+
+file_name_0 = ""
+
+
